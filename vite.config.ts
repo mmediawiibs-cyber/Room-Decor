7375d9ff953025ff3cb3350c958a69b147522e63
Afghani <mmediawiibs@gmail.com>
2026-08-15 18:24:06 +0700

    lll

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Menaikkan batas peringatan dari 500kB ke 1000kB
-  },
+  plugins: [react(), tailwindcss()],
 })
